b6caa6764c249fe463e23432b41e4e4374be71e2
krzys <k.ciolek93@gmail.com>
2019-06-14 08:47:07 +0200

    poprawiony CRUD z dziedziczeniem po AddActivity, dodana migracja

--- app/src/main/java/com/example/aplikacjaism/database/AppDatabase.java
+++ app/src/main/java/com/example/aplikacjaism/database/AppDatabase.java
@@ -1,8 +1,10 @@
 package com.example.aplikacjaism.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 
 @Database(entities = {Pizza.class}, version =4 , exportSchema = false)
@@ -11,10 +13,24 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract PizzaDao pizzaDao();
 
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+
+        }
+    };
+
+    static final Migration MIGRATION_2_1 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+
+        }
+    };
+
     public static AppDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "database-name").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+                    AppDatabase.class, "database-name").allowMainThreadQueries().addMigrations(MIGRATION_1_2, MIGRATION_2_1).build();
         }
         return INSTANCE;
     }
