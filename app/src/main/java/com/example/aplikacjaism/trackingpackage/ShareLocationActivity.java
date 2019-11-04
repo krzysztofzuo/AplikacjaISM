29f5e78ecb208714b65db924a4a74329031de9fe
HaoticV <jacekboguta@gmail.com>
2019-11-04 09:39:13 +0100

    onLocationChanged fix

--- app/src/main/java/com/example/aplikacjaism/trackingpackage/ShareLocationActivity.java
+++ app/src/main/java/com/example/aplikacjaism/trackingpackage/ShareLocationActivity.java
@@ -1,19 +1,22 @@
 package com.example.aplikacjaism.trackingpackage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.aplikacjaism.R;
 
-public class ShareLocationActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class ShareLocationActivity extends AppCompatActivity implements LocationListener {
     private String key;
     private LocationListener mLocationListener;
     private Context context;
@@ -26,44 +29,42 @@ public class ShareLocationActivity extends AppCompatActivity {
         setContentView(R.layout.share_location_activity);
         key = getIntent().getStringExtra("key");
 
-        txtLat = (TextView) findViewById(R.id.lokacja);
+        txtLat = findViewById(R.id.lokacja);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,},
+                    11);
+            if (Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
             return;
         }
-
-
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0, (LocationListener) this);
 
-        mLocationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(final Location location) {
-                txtLat = (TextView) findViewById(R.id.lokacja);
-                txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-            }
+    }
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
 
-            @Override
-            public void onProviderEnabled(String provider) {
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-            }
+    }
 
-            @Override
-            public void onProviderDisabled(String provider) {
+    @Override
+    public void onProviderEnabled(String provider) {
 
-            }
-        };
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
